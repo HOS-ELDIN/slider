5a887cf0740cff10490db752ca03822865f112f1
HOSSAM-ELDIN <HOS-ELDIN@users.noreply.github.com>
2023-02-09 13:06:06 +0200

    final

--- main.js
+++ main.js
@@ -1,29 +1,34 @@
-let sliderStyle = document.querySelector("input:checked + label").innerText;
-let sliderContainer = document.querySelector(".slider-container")
+let sliderStyle = document
+  .querySelector("input:checked + label")
+  .innerText.toLowerCase();
+let sliderContainer = document.querySelector(".slider-container");
 
 window.addEventListener("click", (e) => {
   if (e.target.name == "option") {
-    sliderStyle = e.target.id
+    sliderStyle = e.target.id;
   }
   if (sliderStyle == "fade") {
     console.log(`fade true`);
-    document.styleSheets[2].cssRules[3].style.removeProperty("transform")
+    document.styleSheets[2].cssRules[3].style.removeProperty("transform");
     //give these to imges
-    document.styleSheets[2].cssRules[3].style.setProperty("position", "absolute");
-    sliderContainer.style.display = "block"
-    sliderContainer.style.overflow = "auto"
-    activeate(slide)
+    document.styleSheets[2].cssRules[3].style.setProperty(
+      "position",
+      "absolute"
+    );
+    sliderContainer.style.display = "block";
+    sliderContainer.style.overflow = "auto";
+    activeate(slide);
   } else if (sliderStyle == "slide") {
     //give these to container
-    sliderContainer.style.display = "flex"
-    sliderContainer.style.overflow = "hidden"
+    sliderContainer.style.display = "flex";
+    sliderContainer.style.overflow = "hidden";
     document.styleSheets[2].cssRules[3].style.removeProperty("position");
     imgs.forEach((e) => {
-      e.style.opacity = 1
-    })
-    activeate(slide)
+      e.style.opacity = 1;
+    });
+    activeate(slide);
   }
-})
+});
 
 let slideNumber = document.getElementById("slide-number"),
   prev = document.getElementById("prev"),
@@ -45,11 +50,14 @@ window.onload = () => {
   if (sliderStyle == "fade") {
     console.log(`fade true`);
     //give these to imges
-    document.styleSheets[2].cssRules[3].style.setProperty("position", "absolute");
+    document.styleSheets[2].cssRules[3].style.setProperty(
+      "position",
+      "absolute"
+    );
   } else if (sliderStyle == "slide") {
     //give these to container
-    sliderContainer.style.display = "flex"
-    sliderContainer.style.overflow = "hidden"
+    sliderContainer.style.display = "flex";
+    sliderContainer.style.overflow = "hidden";
   }
   activeate(slide);
 };
@@ -63,7 +71,6 @@ function activeate(id) {
       }
     });
   } else if (sliderStyle == "slide") {
-    // if i want fade comment this
     document.styleSheets[2].cssRules[3].style.setProperty(
       "transform",
       `translatex(-${slide * 800}px)`
@@ -85,6 +92,11 @@ next.addEventListener("click", () => {
   if (slide + 1 < imgs.length) {
     slide++;
     activeate(slide);
+    console.log(slide);
+    prev.classList.remove("disabled");
+    if (slide + 1 == imgs.length && sliderStyle == "slide") {
+      next.classList.add("disabled");
+    }
   } else {
     slide = 0;
     activeate(slide);
@@ -94,9 +106,16 @@ prev.addEventListener("click", () => {
   if (slide > 0) {
     slide--;
     activeate(slide);
+    next.classList.remove("disabled");
+    if (slide == 0 && sliderStyle == "slide") {
+      prev.classList.add("disabled");
+    }
   } else {
-    slide = 4;
-    activeate(slide);
+    if (sliderStyle == "slide") {
+    } else {
+      slide = 4;
+      activeate(slide);
+    }
   }
 });
 
